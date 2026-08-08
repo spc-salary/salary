7b666f39b3470f7431a9d0d2b49976fedc3d01c6
spc-salary <william.sy9@gmail.com>
2026-08-08 17:14:03 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,8 +0,0 @@
-/* Public configuration only. Never put passwords or database credentials here. */
-window.SALARY_APP_CONFIG = Object.freeze({
-  apiUrl: 'https://script.google.com/macros/s/AKfycbydj440RMxow9KYC932vqNvEf42Il7uIs2lg02zBvv6xgxVM-PBkQgn30zUZWdCYlee/exec',
-  requestTimeoutMs: 45000,
-  retries: 2,
-  retryDelayMs: 500,
-  analyticsId: 'G-M7V4JDQ2B9'
-});
